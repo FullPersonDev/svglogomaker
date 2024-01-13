3561b7fb91f13e885c85ff6d555a0a0d8b12d9a0
Martin Aguero <martin.aguero84@gmail.com>
2024-01-13 17:06:13 -0600

    added the required packages to the main index.js file, added to the generatelogo.js file, and imported the logoGenerator function member to the index.js.

---
+++ index.js
@@ -0,0 +1,8 @@
+//Install required packages:
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//Import the generatelogo file:
+const logoGenerator = require('./utils/generatelogo');
+
+//Write down questions for the prompt:
---
+++ utils/generatelogo.js
@@ -0,0 +1,11 @@
+
+
+//Create the svg logo generator function
+
+function logoGenerator(answers) {
+
+}
+
+//Export the logoGenerator
+
+module.exports = logoGenerator;
